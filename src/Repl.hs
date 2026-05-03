7fb9bbabf59ef1fb361f4145ce953c0164db5c13
DCA-13 <davidcampos1312@gmail.com>
2026-05-03 19:07:37 +0200

    track expression number in prompt

--- src/Repl.hs
+++ src/Repl.hs
@@ -8,29 +8,30 @@ import System.Console.Haskeline
 import Text.Megaparsec.Error
 
 repl :: IO ()
-repl = runInputT defaultSettings loop
+repl = runInputT defaultSettings (loop 0)
   where
-    loop = do
-      minput <- getInputLine "S> "
+    loop :: Int -> InputT IO ()
+    loop n = do
+      minput <- getInputLine ("In["++show n++"]=")
       case minput of
         Nothing -> outputStrLn "Quitting..." -- Ctrl+D
         Just input -> case words input of
-          [] -> loop
+          [] -> loop n
           [c] | c `elem` [":q", ":quit"] -> outputStrLn "Quitting..."
               | c `elem` [":h", ":help"] -> do
                 outputStrLn "TODO: Help message"
-                loop
+                loop n
           (c:rest) | c `elem` [":p", ":print"] -> do
             case parser (unwords rest) of
               Left err -> outputStrLn ("Parse error: " ++ errorBundlePretty err)
               Right expr -> outputStrLn . show $ expr
-            loop
+            loop (n + 1)
           ((':':_):_) -> do
             outputStrLn "Unknown command (try :h[elp])"
-            loop
+            loop n
           _ -> do
             run input automaticSimplify
-            loop
+            loop (n + 1)
 
 run :: String -> (Expr -> Expr) -> InputT IO ()
 run str f = case parser str of
