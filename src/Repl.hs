e74cf14f9567f7a97923e80b3c7a3a883d47425e
DCA-13 <davidcampos1312@gmail.com>
2026-05-03 18:41:57 +0200

    swap repl to haskeline

--- src/Repl.hs
+++ src/Repl.hs
@@ -2,28 +2,37 @@ module Repl (repl) where
 
 import Expr
 import Parser
+import Pretty
 import Simplify
-import System.IO
+import System.Console.Haskeline
+import Text.Megaparsec.Error
 
 repl :: IO ()
-repl = do
-  putStr "S> "
-  hFlush stdout
-  line <- getLine
-  if line `elem` [":quit", ":q"] then putStrLn "Quitting SAS" else do
-    process line
-    repl
+repl = runInputT defaultSettings loop
+  where
+    loop = do
+      minput <- getInputLine "S> "
+      case minput of
+        Nothing -> outputStrLn "Quitting..." -- Ctrl+D
+        Just input -> case words input of
+          [] -> loop
+          [c] | c `elem` [":q", ":quit"] -> outputStrLn "Quitting..."
+              | c `elem` [":h", ":help"] -> do
+                outputStrLn "TODO: Help message"
+                loop
+          (c:rest) | c `elem` [":p", ":print"] -> do
+            case parser (unwords rest) of
+              Left err -> outputStrLn ("Parse error: " ++ errorBundlePretty err)
+              Right expr -> outputStrLn . show $ expr
+            loop
+          ((':':_):_) -> do
+            outputStrLn "Unknown command (try :h[elp])"
+            loop
+          _ -> do
+            run input automaticSimplify
+            loop
 
-process :: String -> IO ()
-process input = case words input of
-  (":s":rest) ->
-    run (unwords rest) automaticSimplify
-  _ -> print "Unknown command"
-
-pretty :: Expr -> String
-pretty = show
-
-run :: String -> (Expr -> Expr) -> IO ()
+run :: String -> (Expr -> Expr) -> InputT IO ()
 run str f = case parser str of
-  Left err -> print err
-  Right expr -> putStrLn . pretty . f $ expr
+  Left err -> outputStrLn ("Parse error: " ++ errorBundlePretty err)
+  Right expr -> outputStrLn . pretty . f $ expr
